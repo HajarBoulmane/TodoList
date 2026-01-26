2341e2281a06c7df7dfbf9d3215229392c5bc52a
HajarBoulmane <hajarboulmane2003@gmail.com>
2026-01-26 16:22:40 +0100

    CI/CD

--- backend/core/database.py
+++ backend/core/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/tasks_db")
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback_key")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
